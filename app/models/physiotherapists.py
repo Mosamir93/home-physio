ce9be9651787217f27e0954fe2e95223adc79320
mohamed samir <mohamed_elshirbiny@hotmail.com>
2025-01-03 23:44:59 +0200

    creating the main models

---
+++ app/models/physiotherapists.py
@@ -0,0 +1,14 @@
+from base_model import BaseModel, db
+
+
+class Physiotherapists(BaseModel):
+    """Physiotherapists is the class that
+    will contain the physiotherapist info."""
+    __tablename__ = 'physiotherapists'
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
+    location = db.Column(db.String(200), nullable=False)
+    rating = db.Column(db.Float, nullable=True)
