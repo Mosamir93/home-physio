cb086e94d97b03503867e49168cc63617ee3df0e
mohamed samir <mohamed_elshirbiny@hotmail.com>
2025-01-03 23:44:59 +0200

    creating the main models

--- app/models/base_model.py
+++ app/models/base_model.py
@@ -1,2 +1,11 @@
 from datetime import datetime
 from .. import db
+
+
+class BaseModel(db.model):
+    """BaseModel class that will be used as a
+    parent class in all the other classes."""
+    __abstract__ = True
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
---
+++ app/models/patient.py
@@ -0,0 +1,12 @@
+from base_model import BaseModel, db
+
+
+class Patient(BaseModel):
+    """Patient class which will contains
+    the patient's or the user's info."""
+    __tablename__ = 'patients'
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    phone = db.Column(db.String(20), nullable=False)
